153c5ca24014afd075e29add8ff243b1fb796790
Gustavo <kkowakskigustavo@gmail.com>
2025-08-24 11:03:57 -0300

    feat: Cria beans para inicializar RabbitMQ Admin

--- proposta-app/src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
+++ proposta-app/src/main/java/com/pieropan/propostaapp/config/RabbitMQConfiguration.java
@@ -1,7 +1,12 @@
 package com.pieropan.propostaapp.config;
 
+
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -29,5 +34,13 @@ public class RabbitMQConfiguration {
         return QueueBuilder.durable("proposta-concluida.ms-notificacao").build();
     }
 
+    @Bean
+    public RabbitAdmin createRabbitAdmin(ConnectionFactory connectionFactory){
+        return  new RabbitAdmin(connectionFactory);
+    }
 
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> initializeAdmin(RabbitAdmin rabbitAdmin){
+        return event ->  rabbitAdmin.initialize();
+    }
 }
